c37215730cc549fb3b3f1cd31c0e3633793c75b1
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-06-09 14:03:02 -0300

    feat: adding environment variables to switch dataset

--- src/lib/sanity-client.ts
+++ src/lib/sanity-client.ts
@@ -1,8 +1,12 @@
 import { createClient } from "next-sanity";
 
+const PRJECT_ID = process.env.SANITY_PROJECT_ID || "ejmsm84f";
+const DATA_SET = process.env.SANITY_DATASET || "production";
+const API_VERSION = process.env.SANITY_API_VERSION || "2024-01-01";
+
 export const client = createClient({
-  projectId: "ejmsm84f",
-  dataset: "production",
-  apiVersion: "2024-01-01",
+  projectId: PRJECT_ID,
+  dataset: DATA_SET,
+  apiVersion: API_VERSION,
   useCdn: false,
 });
