ea42b1b75b8c3c33444b2a6275a0a09549e27224
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-06-17 15:49:53 -0300

    feat: header adjustments

--- src/components/header/header.view.tsx
+++ src/components/header/header.view.tsx
@@ -8,25 +8,38 @@ export function Header({ title, subtitle, color, className }: BannerProps) {
   return (
     <header
       className={cn(
-        "flex flex-col justify-between gap-4 bg-white px-10 py-10 text-black md:flex-row",
+        "flex flex-col justify-between bg-white px-10 py-12 text-black md:flex-row",
         "bg-[url('/images/fundo.png')] bg-cover bg-center",
         className,
       )}
       style={{ color }}
     >
-      <div className="flex flex-col items-center gap-8 p-2 md:items-start">
-        <h1 className="font-biryani text-center text-4xl font-bold md:text-left md:text-5xl lg:text-6xl">
+      <div className="flex flex-1 flex-col items-center md:items-start">
+        <h1 className="font-biryani w-full text-center text-5xl/relaxed font-bold md:text-left lg:text-7xl/relaxed">
           {title}
         </h1>
-        <h2 className="font-glacial text-center text-2xl font-semibold md:text-left md:text-3xl lg:text-4xl">
-          {subtitle}
-        </h2>
-      </div>
-      <div className="flex flex-col items-center justify-end gap-[10px]">
-        <p className="flex-1 text-xs font-bold text-zinc-800">Realização:</p>
-        <div className="flex gap-8 p-2">
-          <Image src={logoAesas} alt="logo aesas" width={100} height={100} />
-          <Image src={logoSenac} alt="logo senac" width={100} height={100} />
+
+        <div className="flex w-full flex-col justify-between gap-10 md:flex-row md:gap-0">
+          <h2 className="font-glacial self-center text-center text-3xl/relaxed md:text-start lg:text-5xl/relaxed">
+            {subtitle}
+          </h2>
+          <div className="flex flex-col items-center justify-end gap-[10px]">
+            <p className="text-lg font-bold text-zinc-800">Realização:</p>
+            <div className="flex gap-8 p-2">
+              <Image
+                src={logoAesas}
+                alt="logo aesas"
+                width={100}
+                height={100}
+              />
+              <Image
+                src={logoSenac}
+                alt="logo senac"
+                width={100}
+                height={100}
+              />
+            </div>
+          </div>
         </div>
       </div>
     </header>
