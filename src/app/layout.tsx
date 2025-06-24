a39cba0103949ec3cbd23277d8d904cad24a43b4
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-06-24 13:05:34 -0300

    feat: 🎸 adding GoogleAnalytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 
+import { GoogleAnalytics } from '@next/third-parties/google'
 import type { Metadata } from 'next'
 import { Arimo } from 'next/font/google'
 import localFont from 'next/font/local'
@@ -41,6 +42,7 @@ export default function RootLayout({
           {children}
         </div>
       </body>
+      <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID!} />
     </html>
   )
 }
