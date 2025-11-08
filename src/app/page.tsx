431570ea8d1322f93dc46a3a97515336ad40d0b4
Ricardo Ruiz <ricardo.almendro.ruiz@gmail.com>
2025-11-08 19:48:59 -0300

    fix: 🐛 lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,8 @@ import Link from 'next/link'
 
 import { getCalendarsSlug } from '@/service'
 
-
 function formatSlug(slug: string): string {
-  return slug
-    .replace(/-/g, ' ')
-    .replace(/\b\w/g, (char) => char.toUpperCase())
+  return slug.replace(/-/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase())
 }
 
 export default async function Home() {
@@ -14,15 +11,17 @@ export default async function Home() {
 
   return (
     <div className="container mx-auto flex min-h-dvh max-w-[1000px] flex-col py-10">
-      <h1 className='text-4xl font-semibold text-center'>Calendários de Cursos</h1>
-      <ul className="flex flex-col gap-4 bg-zinc-800 p-8 rounded-md mt-8">
+      <h1 className="text-center text-4xl font-semibold">
+        Calendários de Cursos
+      </h1>
+      <ul className="mt-8 flex flex-col gap-4 rounded-md bg-zinc-800 p-8">
         {slugs.map((slug) => (
           <li key={slug}>
             <Link
               href={`/calendario/${slug}`}
-              className="text-2xl text-blue-500  hover:bg-zinc-700 p-2 w-full rounded-md flex gap-2"
-              target='_blank'
-              rel='noopener noreferrer'
+              className="flex w-full gap-2 rounded-md p-2 text-2xl text-blue-500 hover:bg-zinc-700"
+              target="_blank"
+              rel="noopener noreferrer"
             >
               <span>🗓️</span>
               <span>{formatSlug(slug)}</span>
